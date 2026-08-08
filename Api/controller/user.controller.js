3bbf82ef22d0b7b847e2ff266eabb1ec651f9985
Khudaja <your-email@gmail.com>
2026-08-08 14:11:28 +0500

    updatw api

--- Api/controller/user.controller.js
+++ Api/controller/user.controller.js
@@ -1,28 +1,38 @@
-import cloudinary from '../utils/cloudinary.js';
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
-export const uploadAvatar = async (req, res) => {
+export const updateUser = async (req, res, next) => {
+  // Check karein ke token ka user id aur params ki id same hai ya nahi
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+
   try {
-    // Frontend se aane wali image path / base64 string
-    const { image } = req.body; 
+    // Agar password change kar rahe hain toh usay hash karein
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
 
-    const result = await cloudinary.uploader.upload(image, {
-      folder: 'profile_avatars', // Cloudinary me folder name
-      width: 150,
-      crop: 'scale',
-    });
+    // Database mein user update karein
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar, // <-- Yeh Cloudinary / image URL update karega
+        },
+      },
+      { new: true } // Updated data return karne ke liye
+    );
 
-    // Database me save karne ke liye secure URL return karein
-    res.status(200).json({
-      success: true,
-      url: result.secure_url,
-    });
+    // Password rest response se exclude karein
+    const { password, ...rest } = updatedUser._doc;
+
+    // Response send karein
+    res.status(200).json(rest);
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+    next(error);
   }
 };
-
-export const test = (req, res) => {
-    res.json({ 
-        message: 'Api is working!!!' 
-    });
-}
